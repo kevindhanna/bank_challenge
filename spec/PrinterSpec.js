5f39cd44d11268bc8949314133c7164939d5c284
kevindhanna <kevindhanna@gmail.com>
2019-11-18 15:20:14 +0000

    printer returns a transaction

--- spec/PrinterSpec.js
+++ spec/PrinterSpec.js
@@ -1,11 +1,19 @@
 describe('StatementPrinter', function() {
   beforeEach(function() {
     this.statementPrinter = new StatementPrinter
+    this.transactions = [
+      {credit: 500, date: '12-12-2012'}
+    ]
   })
 
   describe('print', function() {
     it('returns a table of transactions in markdown', function() {
-      expect(this.statementPrinter.print()).toEqual("date || credit || debit || balance")
+      expect(this.statementPrinter.print()).toEqual("\ndate || credit || debit || balance\n")
+    })
+    
+    it('returns the given transactionsin the table', function() {
+      expect(this.statementPrinter.print(this.transactions)).toEqual(
+        "\ndate || credit || debit || balance\n12-12-2012 || 500 ||  || \n")
     })
   })
 })
--- src/Printer.js
+++ src/Printer.js
@@ -1,13 +1,24 @@
 (function(exports){
 
+  var HEADER = "date || credit || debit || balance"
+  
   function StatementPrinter() {
 
   }
-
+  
   StatementPrinter.prototype = {
-    print: function() {
-      return "date || credit || debit || balance"
+    print: function(transactions) {
+      if (transactions) {
+        return "\n" + HEADER + makeLine(transactions[0])
+      } else {
+        return "\n" + HEADER + "\n" 
+      }
     }
+  }
+
+  function makeLine(transaction) {
+    var bar = " || "
+    return "\n" + transaction.date + bar + transaction.credit + bar + bar + "\n"
   }
 
   exports.StatementPrinter = StatementPrinter
