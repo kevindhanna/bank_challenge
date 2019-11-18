7f31eea72b8eb27ade66ff880be918846cd65c83
kevindhanna <kevindhanna@gmail.com>
2019-11-18 11:51:02 +0000

    adds eslint

--- spec/AccountSpec.js
+++ spec/AccountSpec.js
@@ -2,15 +2,19 @@ describe('Account', function() {
   beforeEach(function() {
     this.account = new Account
   })
+
   describe('deposit', function() {
     it('returns the new balance', function() {
       expect(this.account.deposit(500)).toEqual(500)
     })
+
     it('returns the new balance', function() {
       expect(this.account.deposit(1000)).toEqual(1000)
     })
+
     it('adds to the existing balance', function() {
       this.account.deposit(500)
+
       expect(this.account.deposit(500)).toEqual(1000)
     })
   })
@@ -19,11 +23,17 @@ describe('Account', function() {
     beforeEach(function() {
       this.account.deposit(2000)
     })
+
     it('returns the new balance', function() {
       expect(this.account.withdraw(500)).toEqual(1500)
     })
+
     it('returns the new balance', function() {
       expect(this.account.withdraw(1000)).toEqual(1000)
+    })
+
+    it('doesnt allow withdrawls that exceed current balance', function() {
+      expect(function() {this.account.withdraw(5000)}).toThrow("Withdrawl amount exceeds current balance")
     })
   })
 })
--- src/Account.js
+++ src/Account.js
@@ -1,12 +1,17 @@
 function Account() {
+
   this.balance = 0
+
 }
 
 Account.prototype = {
+  
   deposit: function(amount) {
     return this.balance += amount
   },
+
   withdraw: function(amount) {
     return this.balance -= amount
   }
+  
 }
