5324104bf72e31a16ab93b77b2c4888494d60c7b
kevindhanna <kevindhanna@gmail.com>
2019-11-19 11:44:31 +0000

    adds addTransaction to transactionHistory

--- public/src/TransactionHistory.js
+++ public/src/TransactionHistory.js
@@ -3,6 +3,27 @@
   function TransactionHistory() {
     this.transactions = []
   }
+  
+  TransactionHistory.prototype = {
+    addTransaction: function(method, amount, date, balance) {
+      this.transactions.unshift({
+        [method]: amount,
+        date: date,
+        balance: balance
+      })
+    }
+  }
+    
+  // function checkDate(date) {
+  //   if (self.transactions.length > 0) {
+  //     var compareDate = new Date(date.split().reverse().join())
+  //     var lastTransactionDate = new Date(self.transactions[0].date.split().reverse().join())
+      
+  //     if (compareDate < lastTransactionDate) {
+  //       throw 'Date cannot be older than last transaction'
+  //     }
+  //   }
+  // }
 
   exports.TransactionHistory = TransactionHistory
 })(this)
--- spec/TransactionHistorySpec.js
+++ spec/TransactionHistorySpec.js
@@ -3,34 +3,23 @@ describe('TransactionHistory', function() {
     this.transactionHistory = new TransactionHistory
   })
 
-  // it('stores the transaction history', function() {
-  //   expect(this.)
-  // })
+  it('stores the transaction history', function() {
+    expect(this.transactionHistory.transactions).toEqual([])
+  })
 
-  // describe('addTransaction', function() {
+  describe('addTransaction', function() {
+    it('adds a credit transaction to the history', function() {
+      this.transactionHistory.addTransaction('credit', 500, '12-12-2012', 500)
+
+      expect(this.transactionHistory.transactions).toEqual([
+        {credit: 500, date: '12-12-2012', balance: 500}
+      ])
+    })
     // it('doesnt allow deposits older than the most recent transaction', function() {
     //   var self = this
     //   this.account.deposit(500, '12-12-2012')
 
     //   expect(function() {self.account.deposit(500, '12-12-2011')}).toThrow('Date cannot be older than last transaction')
-    // })
-    // function checkDate(date) {
-    //   if (self.transactions.length > 0) {
-    //     var compareDate = new Date(date.split().reverse().join())
-    //     var lastTransactionDate = new Date(self.transactions[0].date.split().reverse().join())
-        
-    //     if (compareDate < lastTransactionDate) {
-    //       throw 'Date cannot be older than last transaction'
-    //     }
-    //   }
-    // }
-    // function addTransaction(method, amount, date) {
-    //   self.transactions.unshift({
-    //     [method]: amount,
-    //     date: date,
-    //     balance: self.balance
-    //   })
-    // }
-  
-  // })
+    // })  
+  })
 })
