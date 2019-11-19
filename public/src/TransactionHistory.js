2ff5752607788f6ecce7b8500c667a27e5ea5378
kevindhanna <kevindhanna@gmail.com>
2019-11-19 11:47:49 +0000

    addTransaction checks date before adding

--- public/src/TransactionHistory.js
+++ public/src/TransactionHistory.js
@@ -1,11 +1,14 @@
 (function(exports) {
 
+  var self
   function TransactionHistory() {
     this.transactions = []
+    self = this
   }
   
   TransactionHistory.prototype = {
     addTransaction: function(method, amount, date, balance) {
+      checkDate(date)
       this.transactions.unshift({
         [method]: amount,
         date: date,
@@ -14,16 +17,16 @@
     }
   }
     
-  // function checkDate(date) {
-  //   if (self.transactions.length > 0) {
-  //     var compareDate = new Date(date.split().reverse().join())
-  //     var lastTransactionDate = new Date(self.transactions[0].date.split().reverse().join())
+  function checkDate(date) {
+    if (self.transactions.length > 0) {
+      var compareDate = new Date(date.split().reverse().join())
+      var lastTransactionDate = new Date(self.transactions[0].date.split().reverse().join())
       
-  //     if (compareDate < lastTransactionDate) {
-  //       throw 'Date cannot be older than last transaction'
-  //     }
-  //   }
-  // }
+      if (compareDate < lastTransactionDate) {
+        throw 'Date cannot be older than last transaction'
+      }
+    }
+  }
 
   exports.TransactionHistory = TransactionHistory
 })(this)
--- spec/TransactionHistorySpec.js
+++ spec/TransactionHistorySpec.js
@@ -23,11 +23,12 @@ describe('TransactionHistory', function() {
         {debit: 500, date: '12-12-2012', balance: 500}
       ])
     })
-    // it('doesnt allow deposits older than the most recent transaction', function() {
-    //   var self = this
-    //   this.account.deposit(500, '12-12-2012')
 
-    //   expect(function() {self.account.deposit(500, '12-12-2011')}).toThrow('Date cannot be older than last transaction')
-    // })  
+    it('doesnt allow deposits older than the most recent transaction', function() {
+      var self = this
+      this.transactionHistory.addTransaction('credit', 500, '12-12-2012', 500)
+
+      expect(function() {self.transactionHistory.addTransaction('credit', 500, '12-12-2011', 500)}).toThrow('Date cannot be older than last transaction')
+    })  
   })
 })
